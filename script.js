cc57af9a0fdd85a3082b16e3d4aa118857af2b97
Cody Martin <codemart10@gmail.com>
2019-12-03 16:26:45 -0500

    finished story 1

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 let ball = document.body.querySelector("div")
 let x = 2
 let y = 20
+let xs = 3
+let ys = 2
+
 const speed = setInterval(() => {
   ball.style.top = y + "px";
-  if (y >= 190){
-  y--;
-}
-  if (y <= 0){
-    y++;
+  if (y >= 400 || y <= 0){
+    ys = ys * -1;
   }
-  (y % 2 == 0) ? y = y + 2 : y = y - 2;
+  y = y + ys;
+
+
 
   ball.style.left = x + "px";
-  if (x >= 190){
-  x--;
-}
-  if (x <= 0){
-    x++;
+  if (x >= 400 || x <= 0){
+    xs = xs * -1;
   }
-  (x % 2 == 0) ? x = x + 2 : x = x - 2;
-}, 10);
+  x = x + xs;
+}, 1);
