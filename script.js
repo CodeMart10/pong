c9cdf7bebb2dc872b0b189e78b1abe22f9d2578f
thienqui-communistspy <professionalbusiness505@gmail.com>
2019-12-04 14:14:52 -0500

    Added paddles

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let ys = 2
 
 const speed = setInterval(() => {
   ball.style.top = y + "px";
-  if (y >= 400 || y <= 0){
+  if (y >= 800 || y <= 0){
     ys = ys * -1;
   }
   y = y + ys;
@@ -14,8 +14,51 @@ const speed = setInterval(() => {
 
 
   ball.style.left = x + "px";
-  if (x >= 400 || x <= 0){
+  if (x >= 1500 || x <= 0){
     xs = xs * -1;
   }
   x = x + xs;
 }, 1);
+let paddle1 = document.getElementById('paddle1');
+let paddle2 = document.getElementById('paddle2');
+paddle1.style.top = "100px";
+paddle1.style.left = "-200px";
+paddle2.style.top = "100px";
+paddle2.style.left = "1400px";
+move = 100;
+move2 = 100;
+let paddle1Coordinates = paddle1.getBoundingClientRect();
+let paddle2Coordinates = paddle2.getBoundingClientRect();
+window.addEventListener("keydown", event => {
+  if(event.key == "w") {
+    if(move == 0) {
+      move = move + 25;
+    }
+    move = move - 25;
+    event.preventDefault();
+    paddle1.style.top = move + "px";
+  } else if(event.key == "s") {
+    if(move == 800) {
+      move = move - 25;
+    }
+    move = move + 25;
+    event.preventDefault();
+    paddle1.style.top = move + "px";
+  }
+   if(event.key == "ArrowUp") {
+     if(move2 == 0) {
+       move2 = move2 + 25;
+     }
+    move2 = move2 - 25;
+    event.preventDefault();
+    paddle2.style.top = move2 + "px";
+  } else if(event.key == "ArrowDown") {
+    if(move2 == 800) {
+      move2 = move2 - 25;
+    }
+    move2 = move2 + 25;
+    event.preventDefault();
+    paddle2.style.top = move2 + "px";
+  }
+
+});
