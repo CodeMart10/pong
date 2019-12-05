d12769e457f2ff20c6248aa430f9d64b0fff4317
Cody Martin <codemart10@gmail.com>
2019-12-04 20:35:10 -0500

    Cleaned up QUI'S mess

--- script.js
+++ script.js
@@ -1,79 +1,91 @@
-let ball = document.body.querySelector("div")
-let x = 2
-let y = 20
-let xs = 3
-let ys = 2
-let array = [];
-let ballCoordinate = ball.getBoundingClientRect();
+//Variables holding nodes
+let ballDiv = document.body.querySelector("div")
 let paddle1 = document.getElementById('paddle1');
 let paddle2 = document.getElementById('paddle2');
-paddle1.style.top = "100px";
-paddle1.style.left = "-200px";
-paddle2.style.top = "100px";
-paddle2.style.left = "1400px";
-move = 100;
-move2 = 100;
+
+//Ball properties
+const ball = {
+  height: 100,
+  width: 100,
+  xDiff: 3,
+  yDiff: 2,
+  speed: 1,
+};
+
+//Ball and paddles starting position
+let x = 2;
+let y = 20;
+let move = 100;
+let move2 = 100;
+
+//Ball and paddles live coordinates
+let ballCoordinate = ballDiv.getBoundingClientRect();
 let paddle1Coordinates = paddle1.getBoundingClientRect();
 let paddle2Coordinates = paddle2.getBoundingClientRect();
-const speed = setInterval(() => {
-  ball.style.top = y + "px";
+
+//array for Paddles live coordinates
+let array = [];
+
+//Clock
+const time = setInterval(() => {
+
+  //y-axis
+  ballDiv.style.top = y + "px";
   if (y >= 800 || y <= 0){
-    ys = ys * -1;
+    ball.yDiff *= -1;
   }
-  y = y + ys;
-console.log(move);
-  ball.style.left = x + "px";
+  y += ball.yDiff;
+
+  //x-axis
+  ballDiv.style.left = x + "px";
   if (x >= 1500 || x <= 0){
-    xs = xs * -1;
+    ball.xDiff *= -1;
   }
-  x = x + xs;
-  console.log(paddle1Coordinates.x);
-  console.log(paddle2Coordinates.x);
-  console.log(array);
-}, 1);
+  x += ball.xDiff;
+
+}, ball.speed);
+
+
 window.addEventListener("keydown", event => {
+  event.preventDefault();
+
+//Up for left paddle
   if(event.key == "w") {
-    console.log(array);
     if(move == 0) {
-      move = move + 25;
+      move += 25;
     }
     move = move - 25;
-    event.preventDefault();
     paddle1.style.top = move + "px";
     array[0] = move;
-    console.log(array[0]);
-    console.log(array);
-  } else if(event.key == "s") {
-    if(move == 800) {
-      move = move - 25;
-    }
-    move = move + 25;
-    event.preventDefault();
-    paddle1.style.top = move + "px";
-    array[0] = move;
-    console.log(array[0]);
-    console.log(array);
-  }
-   if(event.key == "ArrowUp") {
-     if(move2 == 0) {
-       move2 = move2 + 25;
-     }
-    move2 = move2 - 25;
-    event.preventDefault();
-    paddle2.style.top = move2 + "px";
-    array[1] = move2;
-    console.log(array[1]);
-    console.log(array);
-  } else if(event.key == "ArrowDown") {
-    if(move2 == 800) {
-      move2 = move2 - 25;
-    }
-    move2 = move2 + 25;
-    event.preventDefault();
-    paddle2.style.top = move2 + "px";
-    array[1] = move2;
-    console.log(array[1]);
-    console.log(array);
   }
 
+//Down for left paddle
+  if(event.key == "s") {
+    if(move == 800) {
+      move -= 25;
+    }
+    move += 25;
+    paddle1.style.top = move + "px";
+    array[0] = move;
+  }
+
+//Up for right paddle
+  if(event.key == "ArrowUp") {
+    if(move2 == 0) {
+       move2 += 25;
+     }
+     move2 -= 25;
+     paddle2.style.top = move2 + "px";
+     array[1] = move2;
+  }
+
+//Down for right paddle
+  if(event.key == "ArrowDown") {
+    if(move2 == 800) {
+      move2 -= 25;
+    }
+    move2 = move2 + 25;
+    paddle2.style.top = move2 + "px";
+    array[1] = move2;
+  }
 });
