8d505aaf62925c5596d4dbd3d5f3318e0c3b3539
Cody Martin <codemart10@gmail.com>
2019-12-06 16:23:57 -0500

    small fixes

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ let body = document.querySelector("section");
 let bodyCoordinates = body.getBoundingClientRect();
 //Ball properties
 const ball = {
-  height: 50,
-  width: 50,
-  xDiff: 0.5,
-  yDiff: 0.5,
+  height: 7,
+  width: 7,
+  xDiff: .5,
+  yDiff: .5,
   speed: 1
 };
 
 let pads = {
   x1: 0,
   x2: 590,
-  height: 50,
-  width: 10,
-  speed1: 25,
-  speed2: 25
+  height: 25,
+  width: 5,
+  speed1: 15,
+  speed2: 15
 }
 
 
@@ -29,9 +29,10 @@ let y = 20;
 let move = 0;
 let move2 = 0;
 
+//border
 const border = {
-  height: 300,
-  width: 600
+  height: 200,
+  width: 400
 }
 
 //Clock
@@ -40,8 +41,6 @@ const time = setInterval(() => {
   ballDiv.style.top = y + "px";
   if (y + ball.height >= border.height || y <= 0){
     ball.yDiff *= -1;
-
-
   }
 
   //x-axis
@@ -50,32 +49,32 @@ const time = setInterval(() => {
     ball.xDiff *= -1;
   }
 
-  if (move2 < y + ball.height){
-    if(move2 + pads.height >= y){
-      if(x + ball.width >= pads.x2){
+  //Paddle 2
+  if ((move2 < y + ball.height) &&
+      (move2 + pads.height >= y) &&
+      (x + ball.width >= pads.x2)){
         ball.xDiff *= -1;
       }
-    }
-  }
   //Paddle1
-  if(pads.x1 + pads.width >= x) {
-    if (move < y + ball.height){
-      if(move + pads.height >= y) {
+  if((pads.x1 + pads.width >= x) &&
+     (move < y + ball.height) &&
+     (move + pads.height >= y)){
           ball.xDiff *= -1;
         }
-    }
-  }
+
+//ball slope
 y += ball.yDiff;
 x += ball.xDiff;
+
 }, ball.speed);
 
-
+//Keys pressed
 window.addEventListener("keydown", event => {
   event.preventDefault();
 
 //Up for left paddle
   if(event.key == "w") {
-    if(move == 0) {
+    if(move <= 0) {
       move += pads.speed1;
     }
     move -= pads.speed1;
@@ -84,7 +83,7 @@ window.addEventListener("keydown", event => {
 
 //Down for left paddle
   if(event.key == "s") {
-    if(move == border.height - pads.height) {
+    if(move >= border.height - pads.height) {
       move -= pads.speed1;
     }
     move += pads.speed1;
@@ -93,7 +92,7 @@ window.addEventListener("keydown", event => {
 
 //Up for right paddle
   if(event.key == "ArrowUp") {
-    if(move2 == 0) {
+    if(move2 <= 0) {
        move2 += pads.speed2;
     }
     move2 -= pads.speed2;
@@ -103,7 +102,7 @@ window.addEventListener("keydown", event => {
 //Down for right paddle
   if(event.key == "ArrowDown") {
 
-    if(move2 == border.height - pads.height) {
+    if(move2 + pads.height >= border.height) {
       move2 -= pads.speed2;
     }
     move2 = move2 + pads.speed2;
