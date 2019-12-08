76133c2d432f5d03e964dd64c786c0e27b1e6ff0
Cody Martin <codemart10@gmail.com>
2019-12-08 09:17:47 -0500

    small clean up

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ let ballDiv = document.getElementById("ball");
 let paddle1 = document.getElementById('paddle1');
 let paddle2 = document.getElementById('paddle2');
 let body = document.querySelector("section");
-let bodyCoordinates = body.getBoundingClientRect();
+
 //Ball properties
 const ball = {
   height: 7,
   width: 7,
-  xDiff: .5,
+  xDiff: 1,
   yDiff: .5,
   speed: 1
 };
 
+//Paddles propeties
 let pads = {
   x1: 0,
   x2: 590,
@@ -22,45 +23,47 @@ let pads = {
   speed2: 15
 }
 
-
-//Ball and paddles starting position
-let x = 200;
-let y = 20;
-let move = 0;
-let move2 = 0;
-
-//border
+//Border
 const border = {
   height: 200,
   width: 400
 }
 
+//Ball and paddles starting/live position
+let x = 200;
+let y = 20;
+let padY1 = 0;
+let padY2 = 0;
+
+
 //Clock
 const time = setInterval(() => {
+
   //y-axis
   ballDiv.style.top = y + "px";
   if (y + ball.height >= border.height || y <= 0){
-    ball.yDiff *= -1;
+      ball.yDiff *= -1;
   }
 
   //x-axis
   ballDiv.style.left = x + "px";
   if (x + ball.width >= border.width || x <= 0){
-    ball.xDiff *= -1;
+      ball.xDiff *= -1;
   }
 
   //Paddle 2
-  if ((move2 < y + ball.height) &&
-      (move2 + pads.height >= y) &&
+  if ((padY2 < y + ball.height) &&
+      (padY2 + pads.height >= y) &&
       (x + ball.width >= pads.x2)){
         ball.xDiff *= -1;
-      }
+  }
+
   //Paddle1
   if((pads.x1 + pads.width >= x) &&
-     (move < y + ball.height) &&
-     (move + pads.height >= y)){
+     (padY1 < y + ball.height) &&
+     (padY1 + pads.height >= y)){
           ball.xDiff *= -1;
-        }
+  }
 
 //ball slope
 y += ball.yDiff;
@@ -72,40 +75,40 @@ x += ball.xDiff;
 window.addEventListener("keydown", event => {
   event.preventDefault();
 
-//Up for left paddle
+//Up for paddle 1
   if(event.key == "w") {
-    if(move <= 0) {
-      move += pads.speed1;
+    if(padY1 <= 0) {
+      padY1 += pads.speed1;
     }
-    move -= pads.speed1;
-    paddle1.style.top = move + "px";
+    padY1 -= pads.speed1;
+    paddle1.style.top = padY1 + "px";
   }
 
-//Down for left paddle
+//Down for paddle 1
   if(event.key == "s") {
-    if(move >= border.height - pads.height) {
-      move -= pads.speed1;
+    if(padY1 >= border.height - pads.height) {
+      padY1 -= pads.speed1;
     }
-    move += pads.speed1;
-    paddle1.style.top = move + "px";
+    padY1 += pads.speed1;
+    paddle1.style.top = padY1 + "px";
   }
 
-//Up for right paddle
+//Up for paddle 2
   if(event.key == "ArrowUp") {
-    if(move2 <= 0) {
-       move2 += pads.speed2;
+    if(padY2 <= 0) {
+       padY2 += pads.speed2;
     }
-    move2 -= pads.speed2;
-    paddle2.style.top = move2 + "px";
+    padY2 -= pads.speed2;
+    paddle2.style.top = padY2 + "px";
   }
 
-//Down for right paddle
+//Down for paddle 2
   if(event.key == "ArrowDown") {
 
-    if(move2 + pads.height >= border.height) {
-      move2 -= pads.speed2;
+    if(padY2 + pads.height >= border.height) {
+      padY2 -= pads.speed2;
     }
-    move2 = move2 + pads.speed2;
-    paddle2.style.top = move2 + "px";
+    padY2 = padY2 + pads.speed2;
+    paddle2.style.top = padY2 + "px";
   }
 });
